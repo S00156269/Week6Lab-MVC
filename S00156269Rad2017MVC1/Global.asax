97eb95343b95409f106b268d96e2bcfc3d4013de
Ciara <32096395+ciaralynch@users.noreply.github.com>
2017-10-27 10:58:07 +0100

    Add project files.

---
+++ S00156269Rad2017MVC1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="S00156269Rad2017MVC1.MvcApplication" Language="C#" %>
---
+++ S00156269Rad2017MVC1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(S00156269Rad2017MVC1.Startup))]
+namespace S00156269Rad2017MVC1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
